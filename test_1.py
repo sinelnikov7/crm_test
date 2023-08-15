4a0a0bdc06cf799e5779953b01fee3ac6f3c946a
sinelnikov7 <sinelnikovby@gmail.com>
2023-08-15 16:15:37 +0300

    5

--- test_1.py
+++ test_1.py
@@ -82,7 +82,7 @@ def get_new_password():
 
 # @allure.feature('Авторизация')
 # @allure.story('Авторизация с невалидными данными')
-# @allure.severity('normal')
+# @allure.severity('Normal')
 # def test_auth_with_invalid_data(fox_driver_will_close):
 #     """Авторизация с невалидными данными"""
 #     # browsers = [chrome_driver_will_close, fox_driver_will_close]
@@ -129,7 +129,7 @@ def get_new_password():
 
 # @allure.feature('Авторизация')
 # @allure.story('Авторизация по логину с валидными данными')
-# @allure.severity('blocker')
+# @allure.severity('Blocker')
 # def test_auth_with_login(fox_driver_will_close):
 #     """Авторизация по логину"""
 #     browsers = [fox_driver_will_close]
@@ -156,7 +156,7 @@ def get_new_password():
 
 # @allure.feature('Авторизация')
 # @allure.story('Авторизация по телефону с валидными данными')
-# @allure.severity('blocker')
+# @allure.severity('Blocker')
 # def test_auth_with_phone(fox_driver_will_close):
 #     """Авторизация по номеру телефона"""
 #     browsers = [fox_driver_will_close]
@@ -183,88 +183,94 @@ def get_new_password():
 #             assert message == 'Вы успешно вошли'
 #         browser.close()
 
-@allure.feature('Авторизация')
-@allure.story('Авторизация по почте с валидными данными')
-@allure.severity('blocker')
-def test_auth_with_email(fox_driver_will_close):
-    """Авторизация по email"""
-    browsers = [fox_driver_will_close]
-    for browser in browsers:
-        browser.implicitly_wait(5)
-        browser.get('https://strojregionfilomena.workhere.ru/')
-        with allure.step('Наличие поля "Логин"'):
-            user = browser.find_element(By.ID, "auth-form-login_user")
-        with allure.step('Наличие поля "Пароль"'):
-            password = browser.find_element(By.ID, 'auth-form-login_password')
-        with allure.step('Наличие кнопки "Войти"'):
-            login_button = browser.find_element(By.CSS_SELECTOR, '.ant-btn-block')
-        with allure.step('Заполнение поля "Логин"'):
-            user.send_keys('vouka8@yandex.by')
-        with allure.step('Заполнение поля "Пароль"'):
-            password.send_keys('testtest1')
-        with allure.step('Нажать на кнопку войти'):
-            login_button.click()
-            time.sleep(2)
-            try:
-                message = browser.find_element(By.XPATH, '/html/body/div[2]/div/div/div[1]/div/div/div[2]/div').text
-            except NoSuchElementException:
-                message = 'Авторизация не произошла'
-            assert message == 'Вы успешно вошли'
-        browser.close()
+# @allure.feature('Авторизация')
+# @allure.story('Авторизация по почте с валидными данными')
+# @allure.severity('Blocker')
+# def test_auth_with_email(fox_driver_will_close):
+#     """Авторизация по email"""
+#     browsers = [fox_driver_will_close]
+#     for browser in browsers:
+#         browser.implicitly_wait(5)
+#         browser.get('https://strojregionfilomena.workhere.ru/')
+#         with allure.step('Наличие поля "Логин"'):
+#             user = browser.find_element(By.ID, "auth-form-login_user")
+#         with allure.step('Наличие поля "Пароль"'):
+#             password = browser.find_element(By.ID, 'auth-form-login_password')
+#         with allure.step('Наличие кнопки "Войти"'):
+#             login_button = browser.find_element(By.CSS_SELECTOR, '.ant-btn-block')
+#         with allure.step('Заполнение поля "Логин"'):
+#             user.send_keys('vouka8@yandex.by')
+#         with allure.step('Заполнение поля "Пароль"'):
+#             password.send_keys('testtest1')
+#         with allure.step('Нажать на кнопку войти'):
+#             login_button.click()
+#             time.sleep(2)
+#             try:
+#                 message = browser.find_element(By.XPATH, '/html/body/div[2]/div/div/div[1]/div/div/div[2]/div').text
+#             except NoSuchElementException:
+#                 message = 'Авторизация не произошла'
+#             assert message == 'Вы успешно вошли'
+#         browser.close()
 #
-#
+# @allure.feature('Восстановление пароля')
+# @allure.story('Восстановление пароля по логину')
+# @allure.severity('Critical')
 # def test_recover_password_with_login(fox_driver_will_close):
 #     """Восстановление пароля по логину"""
 #     browser = fox_driver_will_close
-#     browser.implicitly_wait(20)
+#     browser.implicitly_wait(10)
 #     browser.get('https://strojregionfilomena.workhere.ru/')
-#     link = browser.find_element(By.XPATH, '//*[@id="root"]/div/div/section/section/section/main/div/div/form/div[1]/span')
-#     link.click()
-#     input_login = browser.find_element(By.XPATH, '//*[@id="auth-form-restore-password_login"]')
-#     input_login.send_keys('admin')
-#     button = browser.find_element(By.XPATH, '//*[@id="root"]/div/div/section/section/section/main/div/div/form/div[1]/div[2]/div/div/span/button')
-#     button.click()
-#     input_key = browser.find_element(By.XPATH, '//*[@id="auth-form-restore-password_code"]')
-#     key = get_key()
-#     input_key.send_keys(key)
-#     button = browser.find_element(By.XPATH, '/html/body/div/div/div/section/section/section/main/div/div/form/div[1]/div[2]/div/div/span/button')
-#     button.click()
+#     with allure.step('Наличие линка "Забыли пароль?" и его нажатие'):
+#         browser.find_element(By.XPATH, '//div[@id="root"]/div/div/section/section/section/main/div/div/form/div[1]/span').click()
+#     with allure.step('Наличие инпута восстановления по логину и ввод в него данных'):
+#         browser.find_element(By.ID, 'auth-form-restore-password_login').send_keys('admin')
+#     with allure.step('Наличие кнопки "Отправить" и ее нажатие'):
+#         browser.find_element(By.CSS_SELECTOR, '.ant-btn-primary').click()
+#     with allure.step('Наличие инпута для подтверждения кода'):
+#         input_key = browser.find_element(By.ID, 'auth-form-restore-password_code')
+#     with allure.step('Получение кода из почты и его ввод в инпут подтверждения'):
+#         key = get_key()
+#         input_key.send_keys(key)
+#     with allure.step('Наличие кнопки "Отправить" и ее нажатие'):
+#         browser.find_element(By.CSS_SELECTOR, '.ant-btn-primary').click()
 #     time.sleep(5)
-#     try:
-#         is_valid = WebDriverWait(browser, 20).until(
-#                     EC.presence_of_element_located((By.XPATH, '/html/body/div[3]/div/div[2]/div/div[2]/div[1]/div/div/div/div')))
-#     except Exception:
-#         browser.close()
-#     assert is_valid.is_displayed()
-#     button_aprove = browser.find_element(By.XPATH, '/html/body/div[3]/div/div[2]/div/div[2]/div[2]/form/div[2]/button[1]')
-#     button_aprove.click()
+#     with allure.step('Потдверждение авторизации после отправки кода'):
+#         try:
+#             is_valid = WebDriverWait(browser, 20).until(
+#                         EC.presence_of_element_located((By.XPATH, '/html/body/div[3]/div/div[2]/div/div[2]/div[1]/div/div/div/div')))
+#             assert is_valid.is_displayed()
+#         except Exception:
+#             browser.close()
+#     with allure.step('Прнименение сгенерированного пароля'):
+#         button_aprove = browser.find_element(By.XPATH, '/html/body/div[3]/div/div[2]/div/div[2]/div[2]/form/div[2]/button[1]')
+#         button_aprove.click()
 #     browser.close()
 #
 #
-# def test_auth_with_new_login(fox_driver_will_close):
-#     """Авторизация с новым паролем, и установка стандартного пароля"""
-#     browser = fox_driver_will_close
-#     browser.implicitly_wait(5)
-#     browser.get('https://strojregionfilomena.workhere.ru/')
-#     user = browser.find_element(By.ID, "auth-form-login_user")
-#     password = browser.find_element(By.ID, 'auth-form-login_password')
-#     login_button = browser.find_element(By.CSS_SELECTOR, '.ant-btn-block')
-#     user.send_keys('admin')
-#     password_get = get_new_password()
-#     password.send_keys(password_get)
-#     login_button.click()
-#     time.sleep(2)
-#     message = browser.find_element(By.XPATH, '/html/body/div[2]/div/div/div[1]/div/div/div[2]/div')
-#     assert message.text == 'Вы успешно вошли'
-#     url = 'https://strojregionfilomena.workhere.ru/api/auth/login?_suppress_response_codes=1&expand=partner.partnerHhConfig'
-#     data = {"user":"admin", "password": password_get}
-#     # data = {"user":"admin", "password": 'testtest1'}
-#     response = requests.post(url, data=data).json()
-#     access = response['data']['token']
-#     url_change_password = f'https://api.macroncrm.ru/user/update?id=2&expand=imageLink%2CimageThumbnailLink%2CassignedRights%2Crequisite%2CsignatureObject%2CpassportObjects%2CisWorkObserver%2CimageCropThumbnailLink%2CisGeneralObserver&access-token={access}'
-#     requests.post(url_change_password, data={"macron_web_pass": "testtest1"}).json()
-#     assert get_new_password() == 'testtest1'
-#     browser.close()
+def test_auth_with_new_login(fox_driver_will_close):
+    """Авторизация с новым паролем, и установка стандартного пароля"""
+    browser = fox_driver_will_close
+    browser.implicitly_wait(5)
+    browser.get('https://strojregionfilomena.workhere.ru/')
+    user = browser.find_element(By.ID, "auth-form-login_user")
+    password = browser.find_element(By.ID, 'auth-form-login_password')
+    login_button = browser.find_element(By.CSS_SELECTOR, '.ant-btn-block')
+    user.send_keys('admin')
+    password_get = get_new_password()
+    password.send_keys(password_get)
+    login_button.click()
+    time.sleep(2)
+    message = browser.find_element(By.XPATH, '/html/body/div[2]/div/div/div[1]/div/div/div[2]/div')
+    assert message.text == 'Вы успешно вошли'
+    url = 'https://strojregionfilomena.workhere.ru/api/auth/login?_suppress_response_codes=1&expand=partner.partnerHhConfig'
+    data = {"user":"admin", "password": password_get}
+    # data = {"user":"admin", "password": 'testtest1'}
+    response = requests.post(url, data=data).json()
+    access = response['data']['token']
+    url_change_password = f'https://api.macroncrm.ru/user/update?id=2&expand=imageLink%2CimageThumbnailLink%2CassignedRights%2Crequisite%2CsignatureObject%2CpassportObjects%2CisWorkObserver%2CimageCropThumbnailLink%2CisGeneralObserver&access-token={access}'
+    requests.post(url_change_password, data={"macron_web_pass": "testtest1"}).json()
+    assert get_new_password() == 'testtest1'
+    browser.close()
 
 # def test_5(fox_driver_wont_close):
 #     fox_driver_wont_close.get('https://strojregionfilomena.workhere.ru/')
